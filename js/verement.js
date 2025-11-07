d67effc150fc84509ccef9476c97b83db2db6fc9
lakroune <lakroune00@gmail.com>
2025-11-07 16:38:44 +0100

    Fonctionnalité : Relie les sections entre elles

---
+++ js/verement.js
@@ -0,0 +1,16 @@
+
+// les dom de
+let  inputMontant= document.getElementById('inputMontant')
+let inputMotif =document.getElementById('inputMotif')
+let listBeneficiaire=document.getElementById("listBeneficiaire")
+let verementButton=document.getElementById('verementButton')
+
+// 
+
+verementButton.addEventListener("click",function(){
+    
+})
+
+
+// 
+
