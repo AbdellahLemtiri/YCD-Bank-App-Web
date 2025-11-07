3d88ebcf503f8ad3c1471fb0a513b903e8a71922
lakroune <lakroune00@gmail.com>
2025-11-08 00:16:59 +0100

    ajout fonction qui faire un transaction externe verment vers compte beneficaire

--- js/verement.js
+++ js/verement.js
@@ -20,23 +20,57 @@ inputMotif.addEventListener("keyup", function () {
     else
         change_border_to_error(inputMotif)
 })
-
-
-
-verementButton.addEventListener("click", function () {
-
+listBeneficiaire.addEventListener("keyup", function () {
+    if (listBeneficiaire.value != "NULL")
+        change_border_to_success(inputMotif)
+    else
+        change_border_to_error(inputMotif)
 })
 
+ 
+verementButton.addEventListener("click", function () {
+    if (listBeneficiaire.value != "NULL" && inputMotif.value.length > 3 && inputMontant.value > 0) {
+        if (confirm("vous voullez  ajoute ?")) {
+            let listTransaction = JSON.parse(localStorage.getItem('listTransaction')) || []
+            listTransaction.push(creeTransaction());
+            localStorage.setItem('listTransaction', JSON.stringify(listTransaction))
+        }
+    }
+    else alert("error")
+})
+
+function getBeneficaire(id) {
+    let tabBeneficaires = JSON.parse(localStorage.getItem('tabBeneficaires')) || []
+    let beneficaire = {}
+    tabBeneficaires.forEach(b => {
+        if (b.idBeneficiare == id)
+            beneficaire = b
+    });
+    return beneficaire
+}
+
+function creeTransaction() {
+    let idTransaction = localStorage.getItem("idTransaction") || 1
+    let transaction = {
+        idTransaction: idTransaction,
+        datetransaction: new Date(),
+        beneficaire: getBeneficaire(listBeneficiaire.value),
+        montant: inputMontant.value,
+        motif: inputMotif.value
+    }
+    localStorage.setItem("idTransaction", ++idTransaction)
+    return transaction
+}
 
 // functions
 
 function uotadelisteBenef() {
     let tabBeneficaires = JSON.parse(localStorage.getItem('tabBeneficaires')) || []
     if (tabBeneficaires.length > 0) {
-        listBeneficiaire.innerHTML = ""
+        listBeneficiaire.innerHTML = ' <option value="NULL" disabled selected>choisir un beneficiaire</option>'
         for (let i = 0; i < tabBeneficaires.length; i++) {
             let option = document.createElement('option')
-            option.setAttribute('value',tabBeneficaires[i].nomcmplet)
+            option.setAttribute('value', tabBeneficaires[i].idBeneficiare)
             option.innerText = tabBeneficaires[i].nomcmplet
             listBeneficiaire.appendChild(option)
         }
@@ -45,3 +79,13 @@ function uotadelisteBenef() {
 
 
 
+////////////////////////////////////////////////////////////////////////////////////////
+
+let inputMontantamoi = document.getElementById('inputMontantamoi') 
+let verementButtonamoi = document.getElementById('verementButtonamoi')
+inputMontantamoi.addEventListener("keyup", function () {
+    if (inputMontantamoi.value > 0 && inputMontantamoi.value < 100000)
+        change_border_to_success(inputMontantamoi)
+    else
+        change_border_to_error(inputMontantamoi)
+})
