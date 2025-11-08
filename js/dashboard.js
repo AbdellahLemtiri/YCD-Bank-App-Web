15548b5d97fcedb905a1a26510d26602c0a9da55
lakroune <lakroune00@gmail.com>
2025-11-08 21:42:40 +0100

    ajout fonction chargeinfodecompte pour charger les info au page dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,14 @@
 if (localStorage.getItem("login") != "seccuss")
     window.location.href = "login.html";
 
-
+let idsalut = document.getElementById('idsalut')
 let sectionInfoCompte = document.getElementById('sectionInfoCompte')
+
 chargeinfodecompte()
 function chargeinfodecompte() {
-    let compte= JSON.parse(localStorage.getItem('compte'))
+    let compte = JSON.parse(localStorage.getItem('compte'))
+    let nomcomplet = (compte.genre == "Homme" ? "Mr. " : "M. ") + compte.nom + " " + compte.prenom
+    idsalut.innerText = "Bonjour " + nomcomplet
     let div = document.createElement('div')
     div.setAttribute('class', 'd-flex justify-content-between align-items-center  pb-1')
     div.innerHTML = ` <div class=" ">
@@ -13,12 +16,12 @@ function chargeinfodecompte() {
                         compte de debiter
                     </div>
                     <div class="fs-12">
-                        ${compte.nom}
+                        ${nomcomplet}
                     </div>
                     <div class="fs-12">
                         ${compte.ribComptePrincipal.numeroCompte}
                     </div>
                 </div>
-                <div class="align-self-end fs-12 fw-bold">${compte.sold} </div>`
+                <div class="align-self-end fs-12 fw-bold text-success">${compte.ribComptePrincipal.sold} DH</div>`
     sectionInfoCompte.appendChild(div)
 }
