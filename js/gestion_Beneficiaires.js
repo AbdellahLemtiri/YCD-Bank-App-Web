b88c7e5af576c044ecb2032f169068d9b502165a
ismail lakroune <124296155+lakroune@users.noreply.github.com>
2025-11-08 19:13:55 +0100

    Future/gestion complète bénéficiaires
    * creation: interface de section 'verement'
    
    * creation: interface de section 'Ajouter un Bénéficaire'
    
    * creation: interface de section 'Gestion des Bénéficaires'
    
    * creation: interface de section 'update navBar'
    
    * Fonctionnalité : Relie les sections entre elles
    
    * ajout function creeBenefiaire
    
    * ajout changment sur interface 'form' de verement
    
    * create: function ajout 'beneficaire au list de beneficaire'
    
    * ajout fonction qui faire un transaction externe
    
    * ajout fonction qui affiche les carte de beneficaires avec leur information
    
    * ajout deriction link vers  dashboard si user deja a un compte

---
+++ js/gestion_Beneficiaires.js
@@ -0,0 +1,29 @@
+let interfacelistbeneficaire = document.getElementById('interfacelistbeneficaire')
+
+
+updatecart()
+
+function updatecart() {
+    let listBeneficiaire = JSON.parse(localStorage.getItem('tabBeneficaires'))
+    listBeneficiaire.forEach(bebeficaire => {
+        let div = document.createElement('div')
+        div.setAttribute('class', ' col-11 col-sm-11 col-md-5 col-lg-3 d-flex flex-column justify-content-center align-items-center border-1')
+        div.setAttribute('id', bebeficaire.idBeneficiare)
+        div.innerHTML = `<span>${bebeficaire.nomcmplet}</span>
+                    <span> ${bebeficaire.numerocompte}</span>
+                    <span>${bebeficaire.etat}</span>
+                    <button class="bg-opacity-0 border-0 align-self-end "><img src="../images/Collapse-icon.png"
+                            alt=""></button>
+                    <ul class="d-none list-group ls-none ">
+                        <li class=""><a class="text-decoration-none text-dark fs-12 fw-bold" href="#"> activer</a></li>
+                        <li class=""><a class="text-decoration-none text-dark fs-12 fw-bold" href="#"> modifer</a></li>
+                        <li class=""><a class="text-decoration-none text-dark fs-12 fw-bold" href="#"> supprimer 3</a>
+                        </li>
+                    </ul>`
+        interfacelistbeneficaire.appendChild(div)
+
+    });
+}
+
+// 
+
