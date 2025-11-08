832360b4cc3db275bd6fbf7366b1aaff51f4a931
lakroune <lakroune00@gmail.com>
2025-11-08 22:53:26 +0100

    ajout function a fais une mese a jour sur les donnees dynamique de user nom et solde et num compte au entete des pages

--- js/navigation_section_gestion_beneficiaires.js
+++ js/navigation_section_gestion_beneficiaires.js
@@ -103,3 +103,33 @@ function changeVisibilite(cardVisible, cardInvisible) {
 
 
 
+
+let idsalut = document.getElementById('idsalut')
+let sectionInfoCompte = document.getElementById('sectionInfoCompte') 
+
+chargeinfodecompte()
+function chargeinfodecompte() {
+    let compte = JSON.parse(localStorage.getItem('compte'))
+    let nomcomplet = (compte.genre == "Homme" ? "Mr. " : "M. ") + compte.nom + " " + compte.prenom
+    idsalut.innerText = "Bonjour " + nomcomplet
+    let div = document.createElement('div')
+    div.setAttribute('class', 'd-flex justify-content-between align-items-center  pb-1')
+    div.innerHTML = ` <div class=" ">
+                    <div class="fs-12 fw-bold  text-orange">
+                        compte de debiter
+                    </div>
+                    <div class="fs-12">
+                        ${nomcomplet}
+                    </div>
+                    <div class="fs-12">
+                        ${compte.ribComptePrincipal.numeroCompte}
+                    </div>
+                </div>
+                <div class="align-self-end fs-12 fw-bold text-success">${compte.ribComptePrincipal.sold} DH</div>`
+    sectionInfoCompte.appendChild(div)
+}
+
+
+
+
+
