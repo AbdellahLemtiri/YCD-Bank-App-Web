b2a80718d0a421f07c01c7b1908ec532276f428a
lakroune <lakroune00@gmail.com>
2025-11-08 13:43:18 +0100

    cration: interface page index de sitweb

---
+++ js/index.js
@@ -0,0 +1,17 @@
+btcreation = document.getElementById('btcreation');
+btconnect1 = document.getElementById('btconnect1');
+btconnect2 = document.getElementById('btconnect2');
+
+btcreation.addEventListener('click', function () {
+    // redirection vers la page signup.html
+    window.location.href = "signup.html";
+});
+btconnect1.addEventListener('click', function () {
+    // redirection vers la page login.html
+    window.location.href = "login.html";
+});
+btconnect2.addEventListener('click', function () {
+    // redirection vers la page login.html
+    window.location.href = "login.html";
+});
+
