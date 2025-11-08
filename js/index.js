f42650b83e56fef1a48a5e4e1f17f1751b1711a3
ismail lakroune <124296155+lakroune@users.noreply.github.com>
2025-11-08 15:35:30 +0100

    Future/page index
    * cration: interface page index de sitweb
    
    * cration: ajout footer au page index
    
    * version final de page index 'corrige les liens de autre pages login/signup'

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ btconnect2 = document.getElementById('btconnect2');
 
 btcreation.addEventListener('click', function () {
     // redirection vers la page signup.html
-    window.location.href = "signup.html";
+    window.location.href = "html/signup.html";
 });
 btconnect1.addEventListener('click', function () {
     // redirection vers la page login.html
-    window.location.href = "login.html";
+    window.location.href = "html/login.html";
 });
 btconnect2.addEventListener('click', function () {
     // redirection vers la page login.html
-    window.location.href = "login.html";
+    window.location.href = "html/login.html";
 });
 
