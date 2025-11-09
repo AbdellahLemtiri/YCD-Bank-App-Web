16de40c8c40158f9e722045bbda4e27f29ac7fb1
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2025-11-09 21:52:40 +0100

    ajoute de la validation de la formulaire pour le paiment

--- js/Recharge_Télécom_Internet.js
+++ js/Recharge_Télécom_Internet.js
@@ -6,3 +6,43 @@ recharges.forEach(recharge=>{
      container_recharge.classList.add('d-none');
      container_for_recharge.classList.remove('d-none');
 })});
+const typeR = document.getElementById("typeR");
+const montantR = document.getElementById("montantR");
+const infomontant = document.getElementById('infomontant');
+const phoneR = document.getElementById("phoneR");
+const infonum=document.getElementById('infonum');
+const valideR = document.getElementById('valideR');
+const FavorisR=document.getElementById('FavorisR');
+const container_alias= document.getElementById('container_alias');
+const infoalias = document.getElementById('infoalias');
+const aliasR = document.getElementById('aliasR');
+let cmptR=0;
+FavorisR.addEventListener('click',()=>{
+    container_alias.classList.remove('d-none');
+})
+valideR.addEventListener('click',()=>{
+     if(phoneR.value.length > 10 || phoneR.value.length < 10){
+        infonum.innerHTML = 'N° de phone invalide ! '
+    }
+    else if ( aliasR.value.length > 20 ){
+        infoalias.innerHTML= 'Pas plus de 20 caracteres';
+    }
+    else if (aliasR.value.length < 2){
+        infoalias.innerHTML = 'Pas moins de 2 caracteres'
+    }
+    else{
+ infonum.innerHTML='';
+
+    }
+//      if(montantR.value > 100 || montantR.value < 10){
+//         infomontant.innerHTML = 'montant invalide !'
+//     }
+//     else{
+//  infomontant.innerHTML='';
+//     }
+})
+    
+   
+
+
+
