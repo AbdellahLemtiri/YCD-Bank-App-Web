b9e5f93219d4c64ac052a44e9b1ea4ebb3dd0463
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2025-11-09 02:37:52 +0100

    conneter la section de recharge a la formulaire de paiment js

--- js/Recharge_Télécom_Internet.js
+++ js/Recharge_Télécom_Internet.js
@@ -1,4 +1,7 @@
-recharge = document.getElementsByClassName('recharge');
+const recharge = document.querySelectorAll('recharge');
+const container_recharge = document.getElementById('container_recharge');
+const container_for_recharge = document.getElementById('container_for_recharge');
 recharge.forEach( addEventListener('click') ,()=> {
-    
+     container_recharge.classList.add('d-none');
+     container_for_recharge.classList.remove('d-none');
 });
