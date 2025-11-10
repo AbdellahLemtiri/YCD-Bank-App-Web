572c533fd0136c2c3ad0297de4958316f983b89c
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2025-11-10 10:05:28 +0100

    conneter la section de favorais avec la formulaire

--- js/Recharge_Télécom_Internet.js
+++ js/Recharge_Télécom_Internet.js
@@ -1,48 +1,50 @@
 const recharges = document.querySelectorAll('.recharge');
 const container_recharge = document.getElementById('container_recharge');
 const container_for_recharge = document.getElementById('container_for_recharge');
-recharges.forEach(recharge=>{
-    recharge.addEventListener('click',function(){
-     container_recharge.classList.add('d-none');
-     container_for_recharge.classList.remove('d-none');
-})});
+recharges.forEach(recharge => {
+    recharge.addEventListener('click', function () {
+        container_recharge.classList.add('d-none');
+        container_for_recharge.classList.remove('d-none');
+    })
+});
 const typeR = document.getElementById("typeR");
 const montantR = document.getElementById("montantR");
 const infomontant = document.getElementById('infomontant');
 const phoneR = document.getElementById("phoneR");
-const infonum=document.getElementById('infonum');
+const infonum = document.getElementById('infonum');
 const valideR = document.getElementById('valideR');
-const FavorisR=document.getElementById('FavorisR');
-const container_alias= document.getElementById('container_alias');
+const FavorisR = document.getElementById('FavorisR');
+const container_alias = document.getElementById('container_alias');
 const infoalias = document.getElementById('infoalias');
 const aliasR = document.getElementById('aliasR');
-let cmptR=0;
-FavorisR.addEventListener('click',()=>{
+let cmptR = 0;
+FavorisR.addEventListener('click', () => {
     container_alias.classList.remove('d-none');
+    FavorisR.classList.add('d-none');
 })
-valideR.addEventListener('click',()=>{
-     if(phoneR.value.length > 10 || phoneR.value.length < 10){
+valideR.addEventListener('click', () => {
+    if (phoneR.value.length > 10 || phoneR.value.length < 10) {
         infonum.innerHTML = 'N° de phone invalide ! '
     }
-    else if ( aliasR.value.length > 20 ){
-        infoalias.innerHTML= 'Pas plus de 20 caracteres';
+    else if (aliasR.value.length > 20) {
+        infoalias.innerHTML = 'Pas plus de 20 caracteres';
     }
-    else if (aliasR.value.length < 2){
+    else if (aliasR.value.length < 2) {
         infoalias.innerHTML = 'Pas moins de 2 caracteres'
     }
-    else{
- infonum.innerHTML='';
-
+    else {
+        infonum.innerHTML = '';
+        infoalias.innerHTML = '';
     }
-//      if(montantR.value > 100 || montantR.value < 10){
-//         infomontant.innerHTML = 'montant invalide !'
-//     }
-//     else{
-//  infomontant.innerHTML='';
-//     }
+    //      if(montantR.value > 100 || montantR.value < 10){
+    //         infomontant.innerHTML = 'montant invalide !'
+    //     }
+    //     else{
+    //  infomontant.innerHTML='';
+    //     }
 })
-    
-   
+
+
 
 
 
