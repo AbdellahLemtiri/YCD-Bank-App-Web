3dacb505f4e0f0bd0963673599539cb2c363a209
ismail lakroune <124296155+lakroune@users.noreply.github.com>
2025-11-10 15:41:29 +0100

    Future/dashboard 1 (#18)
    Co-authored-by: AbdellahLemtiri <lemtiriabdellah@gmail.com>

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,24 +1,76 @@
 if (localStorage.getItem("login") != "seccuss")
     window.location.href = "login.html";
 
-
+let idsalut = document.getElementById('idsalut')
 let sectionInfoCompte = document.getElementById('sectionInfoCompte')
+
 chargeinfodecompte()
 function chargeinfodecompte() {
-    let compte= JSON.parse(localStorage.getItem('compte'))
-    let div = document.createElement('div')
-    div.setAttribute('class', 'd-flex justify-content-between align-items-center  pb-1')
-    div.innerHTML = ` <div class=" ">
-                    <div class="fs-12 fw-bold  text-orange">
-                        compte de debiter
-                    </div>
-                    <div class="fs-12">
-                        ${compte.nom}
-                    </div>
-                    <div class="fs-12">
-                        ${compte.ribComptePrincipal.numeroCompte}
-                    </div>
-                </div>
-                <div class="align-self-end fs-12 fw-bold">${compte.sold} </div>`
-    sectionInfoCompte.appendChild(div)
+    let compte = JSON.parse(localStorage.getItem('compte'))
+    let nomcomplet = (compte.genre == "Homme" ? "Mr. " : "M. ") + compte.nom + " " + compte.prenom
+    idsalut.innerText = "Bonjour " + nomcomplet
+    
 }
+
+
+// ====================================::
+
+
+// ===============pour affichier ou disaffichier le solde
+
+const nonaffichier_solde = document.getElementById('nonaffichier_solde');
+const affichier_solde = document.getElementById('affichier_solde');
+let  noeye = document.getElementById('noeye');
+let eye = document.getElementById('eye');
+eye.addEventListener('click',function(){
+    nonaffichier_solde.classList.add('d-none');
+    affichier_solde.classList.remove('d-none');
+});
+noeye.addEventListener('click',function(){
+    nonaffichier_solde.classList.remove('d-none');
+    affichier_solde.classList.add('d-none');
+});
+
+
+// ===============pour affichier ou disaffichier le solde
+
+
+
+const nonaffichier_solde2 = document.getElementById('nonaffichier_solde2');
+const affichier_solde2 = document.getElementById('affichier_solde2');
+let  noeye2 = document.getElementById('noeye2');
+let eye2 = document.getElementById('eye2');
+eye2.addEventListener('click',function(){
+    nonaffichier_solde2.classList.add('d-none');
+    affichier_solde2.classList.remove('d-none');
+});
+noeye2.addEventListener('click',function(){
+    nonaffichier_solde2.classList.remove('d-none');
+    affichier_solde2.classList.add('d-none');
+});
+
+
+
+//========== pour change le compte 
+let  compte_empargne = false;
+let Compte_cheques = true;
+let  pointsc1 = document.getElementById('pointsc1');
+let compte1= document.getElementById('compte1');
+let pointsc2 = document.getElementById('pointsc2');
+let compte2 = document.getElementById('compte2');
+pointsc1.addEventListener('click',function(){
+    compte1.classList.add('d-none');
+    compte2.classList.remove('d-none');
+      compte_empargne = true;
+      Compte_cheques = false;
+  console.log(compte_empargne);
+      console.log(Compte_cheques);
+})
+pointsc2.addEventListener('click',function(){
+    compte1.classList.remove('d-none');
+    compte2.classList.add('d-none');
+     compte_empargne = false;
+      Compte_cheques = true;
+      console.log(compte_empargne);
+      console.log(Compte_cheques);
+})
