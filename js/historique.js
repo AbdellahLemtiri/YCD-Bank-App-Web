1c86971fa43900035f011a85e5533d8313496dc5
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2025-11-14 18:08:43 +0000

    version final historique

--- js/historique.js
+++ js/historique.js
@@ -25,7 +25,7 @@ for (let i = 0; i < historique.length; i++) {
 
     col.innerHTML = 
         '<div class="col fs-12">' +
-            '<div class="card_his card border-1 shadow-sm rounded-3">' +
+            '<div class="  fs-12 card_his card border-1 shadow-sm rounded-3">' +
                 '<div class="card-body p-2 d-flex align-items-center">' +
                     '<div class="flechbg ' + bg_card + ' bg-opacity-10 rounded-circle p-2 me-3 d-flex align-items-center justify-content-center">' +
                         '<i class="' + color_flech + ' fs-5"></i>' +
@@ -33,7 +33,7 @@ for (let i = 0; i < historique.length; i++) {
                     '<div class="flex-grow-1">' +
                         '<div class="d-flex justify-content-between align-items-start">' +
                             '<div>' +
-                                '<h6 class="mb-1 fw-14 ">' + historique[i].motif + '</h6>' +
+                                '<p class="mb-1 fw-14 ">' + historique[i].motif + '</p>' +
                                 '<p class="text-muted small mb-0">' +
                                     date.getFullYear() + '/' +
                                     String(date.getMonth() + 1).padStart(2, '0') + '/' +
